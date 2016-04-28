f12bef382dd2f03500e632b24b005b403eeb8d43
johnlindquist <johnlindquist@gmail.com>
2016-04-27 18:17:36 -0600

    Adding step 5

--- 01/app.ts
+++ 01/app.ts
@@ -2,10 +2,11 @@ import {Component} from 'angular2/core';
 
 @Component({
   selector: 'app',
-  template: `
-    <i class="fa fa-star" aria-hidden="true"></i>
-    Hello World!
-    <i class="fa fa-star" aria-hidden="true"></i>
+  template: `<div class="container">
+  <i class="fa fa-star" aria-hidden="true"></i>
+  Hello World!
+  <i class="fa fa-star" aria-hidden="true"></i>
+</div>
 `
 })
 export class App{}
--- 04/app.ts
+++ 04/app.ts
@@ -3,12 +3,13 @@ import {Home} from './components/home';
 @Component({
   selector: 'app',
   directives: [Home],
-  template: `
+  template: `<div class="container">
     <i class="fa fa-star" aria-hidden="true"></i>
     <span>Hello World!</span>
     <i class="fa fa-star" aria-hidden="true"></i>
     
     <home></home>
+</div>    
 `
 })
 export class App{}
