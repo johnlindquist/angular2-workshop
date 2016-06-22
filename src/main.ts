6cc7a176d9d57d89420fc8b12a0308ff981afdc0
Lukas Ruebbelke <simpul@gmail.com>
2016-06-22 08:30:52 -0700

    fixing IE issue with core-js

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
+import 'core-js';
 import 'reflect-metadata';
 import 'zone.js/dist/zone';
 import 'zone.js/dist/long-stack-trace-zone';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
 import 'font-awesome/fonts/fontawesome-webfont.woff2';
-
 
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import {App} from './app';
